d0732bc2078ee0c0b9b4aa8a8896f5c791251c72
ayushsaini7717 <ayushsaini7717@gmail.com>
2024-10-11 21:09:27 +0530

    pointer events2

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -4,11 +4,14 @@ import './Intro.css';
 import Newatom from '../../atom';
 import { useRecoilValue } from 'recoil';
 
+
+
+
 const Intro = () => {
   const istrue=useRecoilValue(Newatom);
   return <>
   {istrue?<div className='hidden'></div>:<div className='intro__app'>
-      <video controls autoPlay loop src={meal} type="video/mp4"></video>
+      <video controls autoPlay muted loop src={meal} id='myvid' type="video/mp4"></video>
     </div>}
     
   </>
